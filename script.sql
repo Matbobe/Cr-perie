107a755d301896d8682bcbba582152d3e2ec6df4
aBgAmeuR <a.josset@yahoo.com>
2023-12-12 09:27:13 +0100

    create sql script

---
+++ script.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS NEWSLETTER;
+DROP TABLE IF EXISTS GUESTBOOK;
+
+-- Create tables
+CREATE TABLE NEWSLETTER (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(255) NOT NULL,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE GUESTBOOK (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `message` TEXT NOT NULL,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
+
+-- Default data
+INSERT INTO NEWSLETTER (`email`) VALUES
+  ('ApollineGaillou@gmail.com'),
+  ('DexterDescoteaux@gmail.com');
+
+INSERT INTO GUESTBOOK (`name`, `email`, `message`) VALUES
+  ('Apolline Gaillou', 'ApollineGaillou@gmail.com', 'Les crêpes c''est la vie !'),
+  ('Luce Rocheleau', 'LuceRocheleau@gmail.com', 'Superbe vu sur la mer ! Je recommande !');
